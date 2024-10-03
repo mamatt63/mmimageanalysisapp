9dec2a999a8a8d32dd65702dafdb3349074c6436
Mathieu Mallet <mathieu.mallet1@michelin.com>
2024-10-03 13:42:43 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# this is the main function in which we define our webpage  
+def main():
+    st.markdown("# Image Analysis App 🖼️")
+    st.markdown("### This app allows you to extract visual features from images.")
+
+# Init code
+if __name__=='__main__': 
+    main()
